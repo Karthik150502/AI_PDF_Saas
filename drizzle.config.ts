5dd0937b1faed91c080a954af58b13901ef6e281
Karthik150502 <karthikrdycool@gmail.com>
2024-09-03 14:55:04 +0530

    Deleted the connectionString attibute from the drizzle.config.ts file, as it didnt satisfy the config interface.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -9,7 +9,6 @@ export default {
     schema: './src/lib/db/schema.ts',
     dbCredentials: {
         url: "postgresql://neondb_owner:lv4iVrsB6xbd@ep-wispy-paper-a1ir8kbe.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
-        connectionString: process.env.DATABASE_URL!
         // connectionString: "postgresql://neondb_owner:lv4iVrsB6xbd@ep-wispy-paper-a1ir8kbe.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
     },
 
